5cf41d36d94c40907d7ea7e79e68544b86352238
wiffel <wiffel@tilient.net>
2018-03-16 20:34:42 +0100

    changed cs

--- fib.cs
+++ fib.cs
@@ -1,16 +1,12 @@
-// compile: mcs fib.cs
-// run    : mono fib.exe
-
 using System;
+
 class Fibber
 {
-  static int fib(int n)
-  {
-    return (n < 3) ? 1 : fib(n-2) + fib(n-1);
+  static int fib(int n) {
+    return (n < 3) ? 1 : fib(n-1) + fib(n-2);
   }
 
-  static void Main()
-  {
+  static void Main() {
     for (int i = 44; i < 47; i++)
       Console.WriteLine("fib({0}) = {1}", i, fib(i));
   }
