3d3f57854f50a67d3bdd4f3b950d68189f79f25b
wiffel <wiffel@tilient.net>
2018-04-01 20:00:33 +0200

    altered D version

--- fib04.d
+++ fib04.d
@@ -1,6 +1,20 @@
 import std.stdio;
 
-ulong fib(ulong n)
+/*
+
+to run:
+
+rdmd -mcpu=native -O -release fib04.d
+
+to compile:
+
+dmd -O -inline -release -noboundscheck fib04.d
+ldc2 -mcpu=native -O -release -strip-debug fib04.d
+gdc -O3 -o fib04 fib04.d
+
+*/
+
+pure ulong fib(ulong n)
 {
   return  n < 3 ? 1 : fib(n - 1) + fib(n - 2);
 }
